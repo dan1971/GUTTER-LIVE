50e63580b1805fe42b08f607c8722030146fca59
Dan Rollans <dan@spyninjashq.com>
2026-03-04 12:51:02 -0800

    fixed contact form submit

--- contact_form_submit.php
+++ contact_form_submit.php
@@ -10,15 +10,15 @@ require './PHPMailer/src/SMTP.php';
 define('GUSER', 'servicerequest@rainreadyguttersolutions.com'); // GMail username
 define('GPWD', 'MutterGutter33!'); // GMail password
 
-$send_to = "danrollans100@gmail.com;
+$send_to = "danrollans100@gmail.com";
 
 
-$from_name = $_POST['firstname'];
-$phone = $_POST['phone']; 
-$from = $_POST['email'];
-$subject = "Gutter Service Request";
-$service = $_POST['service'];
-$body = $_POST['message'];
+$from_name = $_POST[firstname];
+$phone = $_POST[phone]; 
+$from = $_POST[email];
+$subject = 'Gutter Service Request';
+$service = $_POST[service];
+$body = $_POST[message];
 
 function smtpmailer($to, $from, $from_name, $subject, $body) { 
     global $error;
@@ -34,17 +34,17 @@ function smtpmailer($to, $from, $from_name, $subject, $body) {
     $mail->AddReplyTo($from, $from_name);
     $mail->SetFrom($from, $from);
     $mail->Subject = $subject;
-    $mail->Body = "From: " . $from_name . "\n" . "Email: " . $from  . "Phone Number: " . $phone . "Service Requested: " . $service . "\n" . "Message: " . $body;
+    $mail->Body = 'From:'. $from_name . '\n' . 'Email: ' . $from  . 'Phone Number: ' . $phone . 'Service Requested: ' . $service . '\n' . 'Message: ' . $body;
     $mail->AddAddress($to);
     if(!$mail->Send()) {
         $error = 'Mail error: '.$mail->ErrorInfo; 
-        $response = array("error","There was a problem sending your message.<br>Please reload the page and try again");
+        $response = array('error','There was a problem sending your message.<br>Please reload the page and try again');
         echo json_encode($response);
         return false;
     } else {
         $error = 'Message sent!';
         $userName = json_encode($from_name);
-        $response = array("success",$userName);
+        $response = array('success',$userName);
         echo json_encode($response); //sending response to ajax
         return true;
     }
@@ -60,11 +60,11 @@ $sanitizedEmail = filter_var($from, FILTER_SANITIZE_EMAIL);
 // Validate Email and send or 
 if (filter_var($sanitizedEmail, FILTER_VALIDATE_EMAIL)) {
         // $userName = json_encode($sanitizedFirstName);
-        // $response = array("success",$userName);
+        // $response = array('success',$userName);
         // echo json_encode($response);
     smtpmailer($send_to,$from,$from_name,$subject,$body);
 } else {
-    $response = array("error","Invalid email address. Please check for typos and resend");
+    $response = array('error','Invalid email address. Please check for typos and resend');
     echo json_encode($response);
 }
 
