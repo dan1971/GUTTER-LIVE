9bb5b2421d11a867b1e320c5f0f1a7af4f31cf72
Dan Rollans <danrollans@yahoo.com>
2026-03-06 00:04:49 -0800

    SErialized form

--- js/danJS.js
+++ js/danJS.js
@@ -202,7 +202,8 @@ canvas.height = parentHeight;
     		$('.form-submit-message-container').css('display','grid');
 
 			let formProcessedInputs = [sCname,Cphone,Cemail,Cmessge];
-            console.log("formProcessedInputs= " + formProcessedInputs.serialize(););	
+let serializedformProcessedInputs=formProcessedInputs.serialize();
+            console.log("formProcessedInputs= " + serializedformProcessedInput);	
 
 			// setTimeout(()=>{
 			// sendInfo(Cname,Cphone,Cemail,Cmessge);
