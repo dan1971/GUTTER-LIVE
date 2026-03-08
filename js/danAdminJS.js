cdc351fa4e75dac7fd4721f8d33ef1b83bec4ad9
Dan Rollans <dan@spyninjashq.com>
2026-03-08 05:41:52 -0700

    deleted dirs

--- js/danAdminJS.js
+++ js/danAdminJS.js
@@ -1,82 +1,6 @@
 $(document).ready(function(){
 
 
-     
-	   (function($) {
-	   $.sanitize = function(input) {
-		let output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-		replace(/<[\/\!]*?[^<>]*?>/gi, '').
-		replace(/<style[^>]*?>.*?<\/style>/gi, '').
-		replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '').
-        replace(/&nbsp;/g, '');
-	    return output;
-	    };
-	 })(jQuery);
-	 let sCname;
-
-     //SANITIZE FIRST NAME END
-
-	  //ON CLICKKKKKKKKKKK
-      $("#contact-form-submit").on("click", (e)=>{
-		e.preventDefault();
-	   let Cname = $('input[name="name"]').val();;
-	   let Cphone =  $('input[name="phone"]').val();
-	   let Cemail =  $('input[name="email"]').val();
-	   let Cmessge = $('#cfMessage').val();
-
-       let formData = $('#contact-form').serialize();
-	   console.log("serialized form values= " + formData);
-
-	   let isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(Cemail);
-
-	  let errors = [0,0,0,0];
-		if(Cname == ""){
-			$('#Fname').prev().html("<span style='color:red;'>*Enter Name</span>");
-			$('#Fname').css({'border-color':'red','border-width':'2px'});
-			errors[0] = 1;
-		} else {
-			$('#Fname').prev().html("");
-			$('#Fname').css({'border-color':'#ccc','border-width':'1px'});
-			sCname = $.sanitize(Cname);
-			errors[0] = 0;
-		} 
-		if(Cemail == ""){
-			$('#email').prev().html("<span style='color: red;'>*Enter Email</span>");
-			$('#email').css({'border-color':'red','border-width':'2px'});
-			errors[2] = 1;
-		} else if(Cemail != "" && isValidEmail == false) {
-			$('#email').prev().html("<span style='color: red;'>*Enter a valid Email</span>");
-			$('#email').css({'border-color':'red','border-width':'2px'});
-			errors[2] = 1;
-		} else {
-			$('#email').prev().html("");
-			$('#email').css({'border-color':'#ccc','border-width':'1px'});
-			errors[2] = 0;
-		}
-
-		let findErrors = errors.includes(1);
-		
-      // CONTACT FORM ERROR CHECK END
-
-		if(!findErrors){
-			let h = $('.form-container-all').height();
-			$('.form-container-all').css('height',h);
-			$('.form-submit-message-container-screen').css('display','grid');
-    		$('.form-submit-message-container').css('display','grid');
-
-	   let formData = $('#contact-form').serialize();
-		console.log(JSON.stringify(formData)); 
-
-			
-			sendInfo(formData);
-				$('#cfName')[0].value = "";  
-				$('#cfPhone')[0].value = ""; 
-				$('#cfEmail')[0].value = "";
-				$('#cfMessage')[0].value="";
-			
-		};
-	  });
-
   sendInfo = (formData)=>{
 	  $.ajax({
         type: 'POST',
@@ -100,8 +24,8 @@ $(document).ready(function(){
                  }
         });}
 
-		// retrieve DB data
 
+	// GET DATA FROM DB
 		$("#viewDB").on("click", (e)=>{
 		e.preventDefault();
 			getCustomerData();
@@ -119,7 +43,7 @@ $(document).ready(function(){
                 $.each(data, function(i, item) {
                     htmlOutput + item.id + "</div>" + htmlOutput + item.FullName + "</div>" + htmlOutput + item.Email + "</div>" + htmlOutput + "</div>" + htmlOutput + item.Phone + "</div>" + htmlOutput + item.ServiceRequested + "</div>" + htmlOutput + item.CustomerMessage + "</div>";
                 });
-                htmlOutput += "</ul>";
+                htmlOutput += "</div>";
                 $("#response-container").html(htmlOutput);
             },
             error: function(xhr, status, error) {
