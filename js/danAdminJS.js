ee1364fd590e7a73a3370524ecb0b684077d8d74
Dan Rollans <dan@spyninjashq.com>
2026-03-09 13:17:23 -0700

    form labels

--- js/danAdminJS.js
+++ js/danAdminJS.js
@@ -53,7 +53,9 @@ $(document).ready(function(){
                 
                 $.each(data, function(index, row) {
 
+                let colHeadDiv = $('<div>').addClass('colHead-row');
                 let rowDiv = $('<div>').addClass('data-row');
+                colHeadDiv.append('<div class="response-field-cell">Name: ' + row.FullName + '</div>');
                     rowDiv.append('<div class="response-field-cell">Name: ' + row.FullName + '</div>');
                      rowDiv.append('<div class="response-field-cell">Email: ' + row.Email + '</div>');
                       rowDiv.append('<div class="response-field-cell">Phone: ' + row.Phone + '</div>');
